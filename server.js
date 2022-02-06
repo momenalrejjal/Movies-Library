94c948fd5b9276b3976fbcb902402513a148e375
moamen <moamenalrejjal@gmail.com>
2022-02-06 12:35:33 +0200

    movies-library

--- server.js
+++ server.js
@@ -2,21 +2,89 @@
 const express = require("express");
 const app = express();
 const data = require("./data.json");
+const axios = require("axios");
+const dotenv = require('dotenv');
+dotenv.config();
+const APIKEY = process.env.APIKEY;
 
+app.use(errorHandler);
 app.get('/HomePage', movieHandler);
 
-app.get('/FavoritePage', favHandlerr);
+app.get('/FavoritePage', favHandler);
 
-function Movie(title, poster_path, overview){
+app.get('/trending', trendHandler);
+
+app.get('/search', searchHandler);
+
+app.get('/TV', tvHandler);
+
+app.get('/reviews', reviewsHandler);
+
+function Movie(id, title, release_date, poster_path, overview){
+    this.id = id;
     this.title = title;
-    this.genre_ids = genre_ids;
+    this.release_date = release_date;
+    this.overview = overview;
     this.poster_path = poster_path;
 }
+
 function movieHandler(req, res){
-    res.status(200).send(data);
+    let movies = [];
+    data.data.map(value=>{
+        let oneMovie = new Movie(value.title, value.poster_path, value.overview,);
+        movies.push(oneMovie);
+        
+    });
+    return res.status(200).json(movies);
 }
-function favHandlerr(req, res){
+function favHandler(req, res){
     res.status(200).send("welcome to favorite page");
+}
+
+function trendHandler(req, res){
+    let moviesT = [];
+    axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=d7b6c540a0400c8b5b5512f2bdb50328').then(value =>{
+    value.data.moviesT.forEach(movie => {
+        let oneMovie = new Movie(movie.id, movie.title, movie.release_date,movie.poster_path ,movie.overview)
+        moviesT.push(oneMovie);
+    })
+    return res.status(200).json(moviesT)
+})
+}
+
+function searchHandler(req, res){
+    let moviesS = [];
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=d7b6c540a0400c8b5b5512f2bdb50328').then(value =>{
+        value.data.moviesS.forEach(movie =>{
+            let oneMovie = new Movie(movie.id, movie.title, movie.release_date,movie.poster_path ,movie.overview)
+            moviesS.push(oneMovie);
+        })
+        return res.status(200).json(moviesS);
+    })
+
+}
+
+function tvHandler(req, res){
+    let moviesTV = [];
+    axios.get('https://api.themoviedb.org/3/tv/634649?api_key=d7b6c540a0400c8b5b5512f2bdb50328').then(value =>{
+        value.data.moviesTV.forEach(movie =>{
+            let oneMovie = new Movie(movie.id, movie.title, movie.release_date,movie.poster_path ,movie.overview)
+            moviesTV.push(oneMovie);
+        })
+        return res.status(200).json();
+    })
+    
+}
+
+function reviewsHandler(req, res){
+    let movieR = [];
+    axios.get('https://api.themoviedb.org/3/review/634649?api_key=d7b6c540a0400c8b5b5512f2bdb50328').then(value =>{
+        value.data.movieR.forEach(movie => {
+            let oneMovie = new Movie(movie.id, movie.title, movie.release_date,movie.poster_path ,movie.overview)
+            movieR.push(oneMovie);
+        })
+        return res.status(200).json();
+    })
 }
 
 app.listen(3000, () => {
