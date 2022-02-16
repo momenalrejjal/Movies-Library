207aceab3a3b8b3c2928f7cd2d2ec086c54adf15
moamen <moamenalrejjal@gmail.com>
2022-02-16 02:34:03 +0200

    solved

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 `use strict`;
 const express = require("express");
 const app = express();
-const data = require("./data.json");
+const data = require("./Movie-Data/data.json");
 const axios = require("axios");
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,26 +10,22 @@ const PORT = process.env.PORT;
 const pg = require("pg");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json()
-const DATABASE_URL = process.env.DATABASE_URL;
-const client = new pg.client(DATABASE_URL);
+const DATABASE_URL = process.env.DATABASE_URL;  
+const client = new pg.Client(DATABASE_URL);
 
 //app.use(errorHandler);
 app.get('/HomePage', movieHandler);
-
 app.get('/FavoritePage', favHandler);
-
 app.get('/trending', trendHandler);
-
 app.get('/search', searchHandler);
-
 app.get('/TV', tvHandler);
-
 app.get('/reviews', reviewsHandler);
-
 app.post("/addmovieslibrary",jsonParser ,addmovieslibraryHandler);
-
 app.get("/getAllMovies", getAllMoviesHan);
 
+app.put("/updatemovie/:id", updatemovieHandler);
+app.delete("/deletemovie/:id", deletemovieHandler);
+app.get("/getmovie/:id", getmovieHandler);
 app.use(express.json());
 function Movie(id, title, release_date, poster_path, overview){
     this.id = id;
@@ -40,25 +36,23 @@ function Movie(id, title, release_date, poster_path, overview){
 }
 
 function movieHandler(req, res){
-    let movies = [];
-    data.data.map(value=>{
-        let oneMovie = new Movie(value.title, value.poster_path, value.overview,);
-        movies.push(oneMovie);
-        
-    });
-    return res.status(200).json(movies);
+        let oneMovie = new Movie(data.title, data.poster_path, data.overview);
+    return res.status(200).json(oneMovie);
 }
+
 function favHandler(req, res){
     res.status(200).send("welcome to favorite page");
 }
 
+//'https://api.themoviedb.org/3/trending/all/day?api_key=d7b6c540a0400c8b5b5512f2bdb50328'
 function trendHandler(req, res){
     let moviesT = [];
-    axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=d7b6c540a0400c8b5b5512f2bdb50328').then(value =>{
-    value.data.moviesT.forEach(movie => {
-        let oneMovie = new Movie(movie.id, movie.title, movie.release_date,movie.poster_path ,movie.overview)
-        moviesT.push(oneMovie);
-    })
+           
+    axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US').then(value =>{
+     value.data.moviesT.forEach(movie => {
+         let oneMovie = new Movie(movie.id, movie.title, movie.release_date,movie.poster_path ,movie.overview)
+         moviesT.push(oneMovie);
+     })
     return res.status(200).json(moviesT)
 })
 }
@@ -100,19 +94,52 @@ function reviewsHandler(req, res){
 
 function addmovieslibraryHandler(req,res){
     let movie = req.body
-    const sql = `INSERT INTO movieslibrary(tital, release_date, overview, poster_path) VALUES ($1, $2, $3, $4) RETURNING * ;`
-    let VALUES = [movie.title, movie.release_date, movie.overview, movie.poster_path]
+    const sql = `INSERT INTO movieslibraryTable(title, release_date, overview, poster_path) VALUES ($1, $2, $3, $4) RETURNING * ;`
+    let values = [movie.title, movie.release_date, movie.overview, movie.poster_path]
 client.query(sql, values).then(() =>{
-    return res.status(201).send("Successful");
+    return res.status(200).send("Successful");
 })
 }
 
 function getAllMoviesHan(req, res){
-    const sql = `SELECT * FROM movieslibrary`;
-    clearInterval.query(sql).then(data => {
-        return res.status(200).json(data.rows);
-    }) 
+    const sql = `SELECT * FROM movieslibraryTable`;
+    client.query(sql).then(data => {
+        return res.status(200).json([data]);
+    })
 }
+
+function updatemovieHandler(req, res){
+const id  = req.params.id;
+const movie = req.body;
+const sql = `UPDATE movieslibraryTable SET title=$1, release_date=$2, poster_path=$3 WHER id=${id} RETURNING *;`
+const values = [movie.title, movie.release_date, movie.poster_path];
+client.query(sql,values).then(data =>{
+   return res.status(200).json(data.rows);
+
+}).catch(error =>{
+    errorHandler(error, req, res);
+})
+};
+
+function deletemovieHandler(req, res){
+ const id =req.params.id;
+ const sql = `DELETE FROM movieslibraryTable WHERE id=${id};`
+ client.query(sql).then(() =>{
+     return res.status(204).json([]);
+ }).catch(error => {
+     errorHandler(error, req, res);
+ })
+};
+
+function getmovieHandler(req, res){
+const id = req.params.id;
+const sql = `SELECT * FROM movieslibraryTable WHERE id=${id};`
+client.query(sql).then((data) =>{
+   return res.status(200).json(data.rows);
+}).catch((error) => {
+    errorHandler(error, req, res);
+})
+};
 
 client.connect().then(() =>
 
@@ -127,39 +154,3 @@ app.listen(3000, () => {
 
 
 
-
-
-//const jsonData = require("./data.json");
-
-/*app.get('/', helloWorldHandler);
-
-app.get("/recipes", recipesHandler);
-
-function Recipe(title, readyInMinutes, summary, vegetarian, instructions, sourceUrl, image, id){
-    this.title = title;
-    this.readyInMinutes = readyInMinutes;
-    this.summary = summary;
-    this.vegetarian = vegetarian;
-    this.instructions = instructions;
-    this.sourceUrl = sourceUrl;
-    this.image = image;
-    this.id = id;
-}
-function helloWorldHandler(req, res){
-    return res.status(200).send("Hello World");
-};
-
-function recipesHandler(req, res){
-    let recipes = [];
-    
-    jsonData.data.map(value => {
-        let oneRecipe = new Recipe(value.title, value.readyInMinutes, value.summary, value.vegetarian, value.instructions, value.sourceUrl, value.image, value.id);
-        recipes.push(oneRecipe);
-    });
-
-    return res.status(200).json(recipes);
-};
-
-app.listen(3000, () => {
-    console.log('Listen to port 3000');
-})*/
